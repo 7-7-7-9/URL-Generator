7622615a2f2f5aba794f745adb32fe961bd99bc9
7-7-7-8 <1234packgoat@gmail.com>
2025-06-23 13:32:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.getElementById('generate-button').addEventListener('click', function() {
-    // Get the URL from the input field
     const url = document.getElementById('url-input').value;
-    // Check if the URL is valid
     if (url) {
-        // Open a new blank page
         const newWindow = window.open('about:blank', '_blank');
-        // Do NOT navigate to the specified URL!
-        // newWindow.location.href = url; // <-- Remove or comment out this line
-        // Show a message to the user
-        document.getElementById('message').style.display = 'block';
+        newWindow.document.write(`
+            <html>
+                <body style="margin:0">
+                    <iframe src="${url}" style="width:100vw; height:100vh; border:none;"></iframe>
+                </body>
+            </html>
+        `);
     } else {
         alert("Please enter a valid URL.");
     }
