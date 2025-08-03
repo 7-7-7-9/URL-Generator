d393c172f58a40236b283dc9f42b4fecc3fc3f46
7-7-7-9 <logan.h77778@gmail.com>
2025-08-03 17:47:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ document.getElementById('generate-button').addEventListener('click', function() 
         const newWindow = window.open('about:blank', '_blank');
         newWindow.document.write(`
             <html>
+                <head>
+                    <title>&#8203;</title> <!-- Zero-width space for a blank title -->
+                    <link rel="icon" href="go/blank.ico"> <!-- Point to your favicon file -->
+                </head>
                 <body style="margin:0">
                     <iframe src="${url}" style="width:100vw; height:100vh; border:none;"></iframe>
                 </body>
             </html>
         `);
+        // Optional: Close the document to finish page load
+        newWindow.document.close();
     } else {
         alert("Please enter a valid URL.");
     }
