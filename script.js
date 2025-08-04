778d965535a11fdccb3b20589ebd4de8fda9c249
7-7-7-9 <logan.h77778@gmail.com>
2025-08-04 07:40:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 document.getElementById('generate-button').addEventListener('click', function() {
     const url = document.getElementById('url-input').value;
     if (url) {
-        const newWindow = window.open('about:blank', '_blank');
-        newWindow.document.write(`
-            <html>
-                <head>
-                    <title>&#8203;</title>
-                    <link rel="icon" href="https://raw.githubusercontent.com/chompypotato/carb0n/refs/heads/main/go/blank.ico" type="image/x-icon">
-                </head>
-                <body style="margin:0; background:#000;">
-                    <iframe src="${url}" style="width:100vw; height:100vh; border:none;"></iframe>
-                </body>
-            </html>
-        `);
-        newWindow.document.close();
-    } else {
-        alert("Please enter a valid URL.");
-    }
+      const newWindow = window.open('about:blank', '_blank');
+if (newWindow) {
+    newWindow.document.title = '\u200B';
+    var link = newWindow.document.createElement('link');
+    link.rel = 'icon';
+    link.href = 'https://raw.githubusercontent.com/chompypotato/carb0n/refs/heads/main/go/blank.ico';
+    newWindow.document.head.appendChild(link);
+
+    var iframe = newWindow.document.createElement('iframe');
+    iframe.src = url;
+    iframe.style.width = "100vw";
+    iframe.style.height = "100vh";
+    iframe.style.border = "none";
+    newWindow.document.body.style.margin = "0";
+    newWindow.document.body.appendChild(iframe);
+} else {
+    alert("Please allow popups.");
+}
+
 });
