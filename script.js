50dc19797b330288db25d73391c62330206a405c
7-7-7-9 <logan.h77778@gmail.com>
2025-08-04 07:33:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ document.getElementById('generate-button').addEventListener('click', function() 
         newWindow.document.write(`
             <html>
                 <head>
-                    <title>&#8203;</title> <!-- Zero-width space for a blank title -->
-                    <link rel="icon" href="https://raw.githubusercontent.com/chompypotato/carb0n/refs/heads/main/go/blank.ico"> <!-- Point to your favicon file -->
+                    <title>&#8203;</title>
+                    <link rel="icon" href="https://raw.githubusercontent.com/chompypotato/carb0n/refs/heads/main/go/blank.ico" type="image/x-icon">
                 </head>
-                <body style="margin:0">
+                <body style="margin:0; background:#000;">
                     <iframe src="${url}" style="width:100vw; height:100vh; border:none;"></iframe>
                 </body>
             </html>
         `);
-        // Optional: Close the document to finish page load
         newWindow.document.close();
     } else {
         alert("Please enter a valid URL.");
