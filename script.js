15900a49375ef0bf0b87e33d4101639e2544aebd
7-7-7-9 <logan.h77778@gmail.com>
2025-08-04 09:07:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 document.getElementById('generate-button').addEventListener('click', function() {
     const url = document.getElementById('url-input').value;
     if (url) {
-      const newWindow = window.open('about:blank', '_blank');
+   const newWindow = window.open('about:blank', '_blank');
 if (newWindow) {
-    newWindow.document.title = '\u200B';
-    var link = newWindow.document.createElement('link');
+    // Ensure the head exists!
+    if (!newWindow.document.head) {
+        const head = newWindow.document.createElement('head');
+        newWindow.document.documentElement.appendChild(head);
+    }
+    
+    // Set a blank favicon
+    const link = newWindow.document.createElement('link');
     link.rel = 'icon';
     link.href = 'https://raw.githubusercontent.com/chompypotato/carb0n/refs/heads/main/go/blank.ico';
     newWindow.document.head.appendChild(link);
 
-    var iframe = newWindow.document.createElement('iframe');
-    iframe.src = url;
+    // Set a blank/hidden title
+    newWindow.document.title = "\u200B"; // zero-width space
+
+    // Add a very simple style to 'force' reflow
+    newWindow.document.body.style.margin = '0';
+
+    // Add iframe
+    const iframe = newWindow.document.createElement('iframe');
+    iframe.src = url; // the url you want to cloak
     iframe.style.width = "100vw";
     iframe.style.height = "100vh";
     iframe.style.border = "none";
-    newWindow.document.body.style.margin = "0";
     newWindow.document.body.appendChild(iframe);
+
+    // Sometimes helps:
+    newWindow.document.close();
 } else {
     alert("Please allow popups.");
 }
+
 
 });
